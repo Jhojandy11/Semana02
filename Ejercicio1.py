e75e62cf0f388b14f637aaf6804d2a47faa5ee67
Jhojandy11 <62601922@continental.edu.pe>
2025-03-29 20:41:47 -0500

    Add files via upload

---
+++ Ejercicio1.py
@@ -0,0 +1,13 @@
+# Objetivo: Area de un rectangulo
+# Nombre; Jhojandy Azariel Michue Izquierdo
+# Fecha: 29/03/2025
+
+# Entrada 
+base=int(input("ingrese la base: "))
+altura=int(input("ingrese la altura: "))
+
+# PROCESO
+calcular = base * altura 
+
+#Salida 
+print("El area del rectangulo con los datos ingresados es: ",calcular)
---
+++ Ejercicio2.py
@@ -0,0 +1,14 @@
+# Objetivo: Volumen de un cubo 
+# Nombre; Jhojandy Azariel Michue Izquierdo
+# Fecha: 29/03/2025
+
+# Entrada 
+LADO=int(input("ingrese el lado: "))
+
+
+# PROCESO
+calcular = LADO ** 3
+
+#Salida 
+
+print("El volumen con los datos ingresados: ",calcular)
